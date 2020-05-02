7dc049dec1b03c61dd449781c10de51af5771de7
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-05-02 10:07:19 +0900

    liquidを使った安全な実装のサンプル

--- liquidTest/src/Lib.hs
+++ liquidTest/src/Lib.hs
@@ -5,5 +5,19 @@ module Lib where
 myDiv :: Int -> Int -> Int
 myDiv = div
 
+safeDiv a 0 = Nothing
+safeDiv a b = Just $ myDiv a b
+
+
+{-@ lazy someFunc @-}
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+	putStrLn "値を入力してください"
+	a <- readLn
+	putStrLn "値を入力してください"
+	b <- readLn
+	case safeDiv a b of
+		Just v -> putStrLn (show v)
+		Nothing -> do
+			putStrLn "0での除算は禁止です．初めから再入力してください"
+			>> someFunc
